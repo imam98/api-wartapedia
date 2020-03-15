97b7a52b20d4664ba92886ac1bbe7d0325cd98aa
imam98 <imannududin@outlook.com>
2020-03-15 17:05:22 +0700

    Add AntaraNews fetch test

--- pkg/infrastructure/news_fetcher/news_fetcher_test.go
+++ pkg/infrastructure/news_fetcher/news_fetcher_test.go
@@ -8,8 +8,50 @@ import (
 	"testing"
 )
 
+func fakeAntaraServer(w http.ResponseWriter, r *http.Request) {
+	responseXML := `
+	<?xml version="1.0" encoding="UTF-8"?>
+	<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/" version="2.0">
+	  <channel>
+		<title>ANTARA News - Berita Terkini</title>
+		<description>News And Service</description>
+		<link>https://www.antaranews.com</link>
+		<language>id</language>
+		<copyright>2020 ANTARA News</copyright>
+		<lastBuildDate>Sun, 15 Mar 2020 15:54:01 +0700</lastBuildDate>
+		<atom:link href="https://www.antaranews.com/rss/terkini.xml" rel="self" type="application/rss+xml"/>
+		<item>
+		  <title>Dummy Title</title>
+		  <link>https://www.antaranews.com/berita/1357722/surabaya-belum-perlu-lockdown-antisipasi-covid-19-sebut-wali-kota</link>
+		  <pubDate>Sun, 15 Mar 2020 15:51:04 +0700</pubDate>
+		  <description><![CDATA[<img src="dummy.jpg">Dummy description]]></description>
+		  <guid isPermaLink="false">https://www.antaranews.com/berita/1357722/surabaya-belum-perlu-lockdown-antisipasi-covid-19-sebut-wali-kota</guid>
+		</item>
+		<item>
+		  <title>Dummy Title</title>
+		  <link>https://www.antaranews.com/foto/1357714/pencegahan-wabah-covid-19-di-kalimantan-tengah</link>
+		  <pubDate>Sun, 15 Mar 2020 15:51:04 +0700</pubDate>
+		  <description><![CDATA[<img src="dummy.jpg">Dummy description]]></description>
+		  <guid isPermaLink="false">https://www.antaranews.com/foto/1357714/pencegahan-wabah-covid-19-di-kalimantan-tengah</guid>
+		</item>
+		<item>
+		  <title>Dummy Title</title>
+		  <link>https://www.antaranews.com/video/1357690/presiden-imbau-masyarakat-bekerja-belajar-dan-beribadah-di-rumah</link>
+		  <pubDate>Sun, 15 Mar 2020 15:51:04 +0700</pubDate>
+		  <description><![CDATA[<img src="dummy.jpg">Dummy description]]></description>
+		  <guid isPermaLink="false">https://www.antaranews.com/video/1357690/presiden-imbau-masyarakat-bekerja-belajar-dan-beribadah-di-rumah</guid>
+		</item>
+	  </channel>
+	</rss>
+	`
+
+	w.Header().Set("content-type", "image/svg+xml")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, responseXML)
+}
+
 func fakeBBCServer(w http.ResponseWriter, r *http.Request) {
-	var responseXML = `
+	responseXML := `
 	<?xml version="1.0" encoding="UTF-8"?>
 	<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
 		<channel>
@@ -60,7 +102,7 @@ func TestBBCNewsFetcher(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(fakeBBCServer))
 	defer server.Close()
 
-	var expectedResult = []news.News{
+	expectedResult := []news.News{
 		news.News{
 			Title:       "Virus corona: Mengapa Indonesia 'tidak terbuka', sementara negara lain bersikap 'transparan'?",
 			Url:         "http://www.bbc.com/indonesia/indonesia-51842758",
@@ -78,6 +120,41 @@ func TestBBCNewsFetcher(t *testing.T) {
 			Url:         "http://www.bbc.com/indonesia/indonesia-51830029",
 			Description: news.Description{Text: "Dummy description"},
 			PubDate:     "Thu, 12 Mar 2020 01:21:54 GMT",
+		},
+	}
+
+	fetcher := NewFetcher()
+	data, err := fetcher.Fetch(server.URL)
+	if err != nil {
+		t.Fatalf("Error occured: %q", err)
+	}
+
+	assertLength(t, expectedResult, data)
+	assertElements(t, expectedResult, data)
+}
+
+func TestAntaraNewsFetcher(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(fakeAntaraServer))
+	defer server.Close()
+
+	expectedResult := []news.News{
+		news.News{
+			Title:       "Dummy Title",
+			Url:         "https://www.antaranews.com/berita/1357722/surabaya-belum-perlu-lockdown-antisipasi-covid-19-sebut-wali-kota",
+			Description: news.Description{Text: "<img src=\"dummy.jpg\">Dummy description"},
+			PubDate:     "Sun, 15 Mar 2020 15:51:04 +0700",
+		},
+		news.News{
+			Title:       "Dummy Title",
+			Url:         "https://www.antaranews.com/foto/1357714/pencegahan-wabah-covid-19-di-kalimantan-tengah",
+			Description: news.Description{Text: "<img src=\"dummy.jpg\">Dummy description"},
+			PubDate:     "Sun, 15 Mar 2020 15:51:04 +0700",
+		},
+		news.News{
+			Title:       "Dummy Title",
+			Url:         "https://www.antaranews.com/video/1357690/presiden-imbau-masyarakat-bekerja-belajar-dan-beribadah-di-rumah",
+			Description: news.Description{Text: "<img src=\"dummy.jpg\">Dummy description"},
+			PubDate:     "Sun, 15 Mar 2020 15:51:04 +0700",
 		},
 	}
 
