9fb10d5ffb365a06b518536aad35e8863bab3ed9
imam98 <imannududin@outlook.com>
2020-03-17 21:51:39 +0700

    Add json tags for news model

--- pkg/news/news.go
+++ pkg/news/news.go
@@ -3,21 +3,21 @@ package news
 import "encoding/xml"
 
 type News struct {
-	ID           string      `xml:"-"`
-	Source       string      `xml:"-"`
-	Title        string      `xml:"title"`
-	Enclosure    Media       `xml:"enclosure,omitempty"`
-	MediaContent Media       `xml:"content,omitempty"`
-	Url          string      `xml:"link"`
-	Description  Description `xml:"description"`
-	PubDate      string      `xml:"pubDate"`
+	ID           string      `xml:"-" json:"id,omitempty"`
+	Source       string      `xml:"-" json:"source,omitempty"`
+	Title        string      `xml:"title" json:"title"`
+	Enclosure    Media       `xml:"enclosure,omitempty" json:"enclosure,omitempty"`
+	MediaContent Media       `xml:"content,omitempty" json:"media_content,omitempty"`
+	Url          string      `xml:"link" json:"url"`
+	Description  Description `xml:"description" json:"description"`
+	PubDate      string      `xml:"pubDate" json:"pub_date"`
 }
 
 type Description struct {
-	XMLName xml.Name `xml:"description"`
-	Text    string   `xml:",cdata"`
+	XMLName xml.Name `xml:"description" json:"-"`
+	Text    string   `xml:",cdata" json:"text"`
 }
 
 type Media struct {
-	Src string `xml:"url,attr"`
+	Src string `xml:"url,attr" json:"src,omitempty"`
 }
